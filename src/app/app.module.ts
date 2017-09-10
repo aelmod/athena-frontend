69571024640bbb88349324192c3c29c8adaa9b88
aelmod <aelmod@mail.ru>
2017-09-10 16:17:44 +0300

    added question and answer component, renamed app tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './answer/answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule
