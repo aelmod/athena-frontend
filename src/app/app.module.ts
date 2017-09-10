216892a8a3d3f002e15e48919730db96a5d55eda
aelmod <aelmod@mail.ru>
2017-09-10 16:29:53 +0300

    added RouteModule and example routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { AnswerComponent } from './answer/answer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  // { path: 'test1', component:  },
+  // { path: 'test2/:id',      component:  },
+  // {
+  //   path: 'test3',
+  //   component: ,
+  //   data: {  }
+  // },
+  // { path: '',
+  //   redirectTo: '/test4',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +28,13 @@ import { AnswerComponent } from './answer/answer.component';
     AnswerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
